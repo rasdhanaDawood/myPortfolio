fabc2fec73870347cbf406686045ad94d6992127
rasdhana <rasdhana@gmail.com>
2025-05-26 16:13:20 +0300

    Language updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import {Hero} from './components/Hero'
 import { useEffect } from 'react';
 import {Skills} from './components/Skills';
 import {Experience} from './components/Experience';
-import { Language } from './components/language';
+import { Language } from './components/Language';
 import { Projects } from './components/Projects';
 import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
